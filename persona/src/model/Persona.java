a4d97a40d6972f9e78b05f51caa719135d0c17e2
Rodolfo Campos <camposer@gmail.com>
2015-05-05 16:23:57 +0200

    Modelo de persona y configuración JPA

---
+++ persona/src/model/Persona.java
@@ -0,0 +1,68 @@
+package model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Date;
+
+
+/**
+ * The persistent class for the PERSONA database table.
+ * 
+ */
+@Entity
+@Table(name="PERSONA")
+@NamedQuery(name="Persona.findAll", query="SELECT p FROM Persona p")
+public class Persona implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(unique=true, nullable=false)
+	private Integer id;
+
+	@Column(nullable=false, length=50)
+	private String apellido;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name="FECHA_NACIMIENTO")
+	private Date fechaNacimiento;
+
+	@Column(nullable=false, length=50)
+	private String nombre;
+
+	public Persona() {
+	}
+
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getApellido() {
+		return this.apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public Date getFechaNacimiento() {
+		return this.fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
