4a70452f958f907f41c7921990499dd1942932e7
Rodolfo Campos <camposer@gmail.com>
2015-05-05 17:36:42 +0200

    Implementación del DAO de Persona con genéricos

---
+++ persona/src/dao/PersonaDaoImpl.java
@@ -0,0 +1,17 @@
+package dao;
+
+import java.util.List;
+
+import model.Persona;
+
+public class PersonaDaoImpl
+		extends GenericDaoImpl<Persona, Integer>
+		implements PersonaDao {
+
+	@Override
+	public List<Persona> obtenerTodosPorNombre(String nombre) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
