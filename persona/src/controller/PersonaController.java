5448fd1a5fb47cdf90357e5732cec4e4d8a0e2c4
Rodolfo Campos <camposer@gmail.com>
2015-05-06 10:09:27 +0200

    Implementación del listar de Persona

--- persona/src/controller/PersonaController.java
+++ persona/src/controller/PersonaController.java
@@ -1,13 +1,21 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import service.PersonaService;
 
 @Controller
 @RequestMapping("/persona/*")
 public class PersonaController {
+	@Autowired
+	private PersonaService personaService;
+	
 	@RequestMapping("index")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("personas", personaService.obtenerPersonas());
 		return "/WEB-INF/persona/index.jsp";
 	}
 }
