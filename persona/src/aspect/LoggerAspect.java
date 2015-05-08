f4816f2f91d305c1f40713a08f72221e98bf6500
Rodolfo Campos <camposer@gmail.com>
2015-05-08 12:35:55 +0200

    Implementación de aspectos con aspectj para PersonaServiceImpl.obtenerPersonas()

--- persona/src/aspect/LoggerAspect.java
+++ persona/src/aspect/LoggerAspect.java
@@ -4,18 +4,22 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import util.LoggerUtil;
 
+@Component
 @Aspect
 public class LoggerAspect {
 	@Autowired
 	private LoggerUtil loggerUtil;
 	
 	@Around("execution(* service.PersonaServiceImpl.obtenerPersonas())")
-	public void logAround(ProceedingJoinPoint joinPoint) throws Throwable {
-		loggerUtil.log(joinPoint.getClass(), 
+	public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+		loggerUtil.log(joinPoint.getTarget().getClass(), 
 				joinPoint.getSignature().getName(), 
 				joinPoint.getArgs());
+		
+		return joinPoint.proceed();
 	}
 }
